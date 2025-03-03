5bf82670010c6522c0c4503625b3994cace602de
Yoel <villayoel@gmail.com>
2025-03-04 00:36:01 +0100

    -Add more themes

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -7,6 +7,8 @@ export class ThemeService {
 
   public themes = [
     'slate','grey','zinc','neutral','stone','red','orange',
+    'amber','yellow','lime','green','emerald','teal','cyan',
+    'sky','blue','indigo','violet' ,'purple', 'fuchsia','pink','rose','pink'
   ]
 
   private currentTheme:string = 'slate'
