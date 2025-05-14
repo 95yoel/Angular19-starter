8f69c2b19fe187457b872bab883eff9dee6f49f1
Yoel <villayoel@gmail.com>
2025-05-14 20:10:14 +0200

    - Add ci

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { appConfig } from './app/app.config'
 import { AppComponent } from './app/app.component'
+import { Logger } from 'ts-logger/src/logger'
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err))
+
+const log = Logger.create('Angular19-starter')
+log.info('working')
